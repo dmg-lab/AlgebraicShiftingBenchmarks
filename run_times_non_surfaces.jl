17bbad8773a9705de1c8639fdbe7ea5e4bdc4c0e
Antony Della Vecchia <vecchia@math.tu-berlin.de>
2025-12-08 15:06:35 +0100

    remove limit and adjust non surfaces

--- run_times_non_surfaces.jl
+++ run_times_non_surfaces.jl
@@ -35,16 +35,20 @@ open(non_surfaces_table_path, "w") do f
   for example_file in readdir(non_surfaces_dir)
     println("Non Surface: $example_file")
     K = load(joinpath(non_surfaces_dir, example_file))
-    # Only shift the hypergraph of the 2-facets of K
-    for q in 2:dim(K)
+    prev_uhg = Dict{Tuple{Int, String}, UniformHypergraph}((F, algo.first) => uniform_hypergraph(Vector{Int}[]) for algo in algorithms for (F, _) in fields)
+    for q in 1:dim(K)
       S = uniform_hypergraph(K, q+1)
       # Initial entries of the row
       timings = [example_file, homology(K, 1), n_vertices(S), length(faces(S)), q]
       # Produce timings for each field and algorithm
       for (F, _) in fields, (algo, labels) in algorithms
-        result = run_function(run_benchmark, S, algo, F; remote=useremote, time_limit=3, finite_field_lv_trials=500)
+        result = run_function(run_benchmark, S, algo, F; remote=useremote, time_limit=3, finite_field_lv_trials=500, lower_uhg=prev_uhg[(F, algo)])
         # Append the results to the timings, or, if computation died or timed out, append correct number of "oom" or "oot" respectively.
         n_columns = length(labels) + length(ref_labels)
+        if !isnothing(result)
+          prev_uhg[(F, algo)] = popfirst!(result)
+        end
+
         append!(timings, isnothing(result) ? fill("oom", n_columns) : result == :timed_out ? fill("oot", n_columns) : result)
       end
       println(f, join(timings, ", "))
--- run_times_surfaces.jl
+++ run_times_surfaces.jl
@@ -37,7 +37,7 @@ println(stderr, "Benchmark can take up to $(time_limit * length(instance_files) 
 open(surfaces_table_path, "w") do f
   # Table headers
   println(f, join(["#, instance", "dim", "nVertices", "nFaces", "orientable", "genus", "index", "q", ["$(f[2])$(uppercasefirst(l))" for f in fields for (algo, label) in algorithms for l in add_ref_labels(algo, label)]...], ", "))
-  for example_file in readdir(surfaces_dir)[1:12]
+  for example_file in readdir(surfaces_dir)
     println("Surface: $example_file")
     K = load(joinpath(surfaces_dir, example_file))
     # Parse the filename into the parameters
