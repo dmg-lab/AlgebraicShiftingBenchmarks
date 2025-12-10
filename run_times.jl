4d4eada88927ed22efe872fea475a8c89a8af60c
Antony Della Vecchia <vecchia@math.tu-berlin.de>
2025-12-10 15:29:10 +0100

    fix run times

--- run_times.jl
+++ run_times.jl
@@ -1,10 +1,13 @@
 # Run this file with (for example):
 #   ulimit -v 80000000 -x 0
-#   julia --heap-size-hint=1 --project=. run_times_other.jl
+#   julia --heap-size-hint=1 --project=. 
 
 
-include("run_times_surfaces.jl")
-include("run_times_non_surfaces.jl")
 include("run_times_bipartite.jl")
 include("run_times_surfaces_extra.jl")
+#include("run_times_other.jl")
+#include("run_times_surfaces.jl")
+include("run_times_non_surfaces.jl")
+
+
 
