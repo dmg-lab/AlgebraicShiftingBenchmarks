fdeaad637aa08d463aab127e826ab2587541a3aa
antonydellavecchia <antonydellavecchia@gmail.com>
2025-12-13 16:31:59 +0100

    updated extra run times

--- run_times_surfaces_extra.jl
+++ run_times_surfaces_extra.jl
@@ -7,13 +7,13 @@ const root_of_project = dirname(Base.active_project())
 surfaces_extra_table_path = joinpath(root_of_project, "surfaces_extra_test_$computer_name.csv")
 
 algorithms = [
-  #"hv"   => ["hvTime", "hvMemory"],
-  "lv" => ["lvfTime", "lvfMemory", "lvfTrials", "lvfTimeA", "lvfMemoryA", "lvfTimeB", "lvfMemory"],
-  #"hvf"  => ["hvfTime", "hvfMemory"],
+  "hv"   => ["hvTime", "hvMemory"],
+  "lv" => ["lvTime", "lvMemory", "lvTrials", "lvTimeA", "lvMemoryA", "lvTimeB", "lvMemory"],
+  "hvf"  => ["hvfTime", "hvfMemory"],
   "lvf"  => ["lvfTime", "lvfMemory", "lvfTrials", "lvfTimeA", "lvfMemoryA", "lvfTimeB", "lvfMemory"]
 ]
 fields = [
-  2 => "F2"
+  4 => "F4"
 ]
 
 useremote = true
@@ -32,15 +32,11 @@ println(stderr, "Benchmark can take up to $(time_limit * length(instance_files) 
 open(surfaces_extra_table_path, "w") do f
   # Table headers
   println(f, join(["#", "instance", "dim", "nVertices", "nFaces", "orientable", "genus", "index", "q", ["$(field)$(uppercasefirst(l))" for (_,field) in fields for (algo, label) in algorithms for l in add_ref_labels(algo, label)]...], ", "))
-  for example_file in readdir(surfaces_dir)
+  for example_file in readdir(surfaces_dir)[12:34]
     K = load(joinpath(surfaces_dir, example_file))
     # Parse the filename into the parameters
     nr, dim, n, orientable, genus, index = match(r"^(\d\d)_manifold_lex_d(\d)_n(\d)_o(\d)_g(\d)_(\d\d)\..*$", example_file).captures
     # Only compute the shifts of the surfaces that remain after the following:
-    if n != "8" || orientable != "1" || genus != "0"
-      continue
-    end
-
     println("Surface: $example_file")
     # Produce timings for each field and algorithm
     prev_uhg = Dict{Tuple{Int, String}, UniformHypergraph}((F, algo.first) => uniform_hypergraph(Vector{Int}[]) for algo in algorithms for (F, _) in fields)
